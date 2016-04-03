f487f318ac7a63e0759267e5cf84be05f4663e4b
Tatyana <Tatyana@Aurora>
2016-04-04 01:14:13 +0300

    добавили много всего

---
+++ src/main/java/dao/SecurityDAO.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import javax.transaction.Transactional;
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import security.Users;
+
+/**
+ *
+ * @author Tatyana
+ */
+
+@Transactional
+public class SecurityDAO {
+
+    SessionFactory sessions = new Configuration().configure().buildSessionFactory();
+
+    public Users thyPass(String uname) {
+
+        Query query = sessions.getCurrentSession().createQuery("FROM Users where username= :uname");
+        if (query.list().get(0) != null) {
+            return (Users) query.list().get(0);
+
+        } else {
+            return null;
+        }
+    }
+
+}
